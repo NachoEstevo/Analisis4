dba7da68a11c00e4c6edc2d94489d34ccfba9f13
Ignacio Estevo <nachoestevo@gmail.com>
2021-08-09 22:13:23 -0300

    Practica 1

--- Practica1.py
+++ Practica1.py
@@ -3,13 +3,13 @@
 
 # <h1>Guia 1 Python Teoria de error</h1>
 
-# In[3]:
+# In[2]:
 
 
 import math
 
 
-# In[4]:
+# In[3]:
 
 
 first = 22/7
@@ -17,7 +17,7 @@ errOne = abs(math.pi - first)
 print(errOne)
 
 
-# In[5]:
+# In[4]:
 
 
 second = 333/106
@@ -25,7 +25,7 @@ errTwo = abs(math.pi - second)
 print(errTwo)
 
 
-# In[6]:
+# In[5]:
 
 
 third = 355/113
@@ -35,7 +35,7 @@ print(errThree)
 
 # Errores relativos y porcentuales  er(x) = e(x)/X  ejemplo 0.00059/3.141 ese es el relativo. Multiplicado por 100 da el porcentual 
 
-# In[7]:
+# In[6]:
 
 
 relOne = errOne/math.pi
@@ -55,7 +55,7 @@ print('porcentual three: ', round(relThree*100,3),'%')
 
 # Calculo de error de otro numero irracional. e
 
-# In[13]:
+# In[7]:
 
 
 firstE = 2.72
@@ -64,7 +64,7 @@ print(errOneE)
 print('Cotas: ',firstE-errOneE,'< e <',firstE + errOneE)
 
 
-# In[15]:
+# In[8]:
 
 
 secondE = 2.718
@@ -73,7 +73,7 @@ print(errTwoE)
 print('Cotas: ',secondE-errTwoE,'< e <',secondE + errTwoE)
 
 
-# In[17]:
+# In[9]:
 
 
 thirdE = 2.7183
@@ -82,7 +82,7 @@ print(errThreeE)
 print('Cotas: ',thirdE-errThreeE,'< e <',thirdE + errThreeE)
 
 
-# In[11]:
+# In[10]:
 
 
 relOneE = errOneE/math.e
@@ -118,7 +118,7 @@ print('porcentual three: ', round(relThreeE*100,3),'%')
 # .Que medida tiene menor error absoluto? .Cual es mas precisa?
 # 
 
-# In[43]:
+# In[11]:
 
 
 lSpeedFoucalt = 298000
@@ -134,7 +134,7 @@ percentError = relError*100
 print(round(percentError,3),'%')
 
 
-# In[45]:
+# In[12]:
 
 
 planck = 6.41*(10**-27)
@@ -154,7 +154,7 @@ print(round(percentError2,3),'%')
 
 # <h2>Ejercicio 3</h2>
 
-# In[25]:
+# In[13]:
 
 
 #Approximate e with the limit
@@ -168,7 +168,7 @@ absErr1 = abs(math.e - val1)
 print('absolute error:',absErr1)
 
 
-# In[26]:
+# In[14]:
 
 
 val2 = aproxE(100)
@@ -178,7 +178,7 @@ absErr2 = abs(math.e - val2)
 print('absolute error:',absErr2)
 
 
-# In[27]:
+# In[15]:
 
 
 val3 = aproxE(10**25) #Se aleja mas del valor esperado. Obtenemos e al resolver la indeterminacion cuando x tiende a inf
@@ -190,7 +190,7 @@ print('absolute error:',absErr3)
 
 # <h2>Ejercicio 4</h2>
 
-# In[53]:
+# In[16]:
 
 
 #Derivada del sen(x)
@@ -208,4 +208,97 @@ for x in xArr:
     for h in hArr:
         print('Sin derivate in:',x, 'with h =',h,'=',round(derivateSin(x,h),3))
         print('Cos derivate in:',x, 'with h =',h,'=',round(derivateCos(x,h),3),'\n')
+
+
+# <h2>Ejercicio 5</h2>
+
+# Escribir y probar un programa para calcular:
+#         f(x) = sqrt(x^2 + 1)-1
+#         g(x) = x^2/sqrt(x^2 + 1)-1
+# para una sucesión de valores de x, como: 8^-1, 8^-2, 8^-3
+# ...Aunque f = g, la computadora dará resultados diferentes.
+# ¿Cuáles de los resultados son de fiar y cuáles no? 
+
+# In[25]:
+
+
+def f(x):
+    return math.sqrt(x**2 + 1)-1
+def g(x):
+    return x**2/math.sqrt(x**2 + 1)-1
+
+xArr = [8**-1,8**-2,8**-3]
+
+for x in xArr:
+    print('f(',x,') = ',f(x))
+    print('g(',x,') = ',g(x))
+    print('\n')
+
+
+# <h2>Ejercicio 6</h2>
+
+# Usar la computadora para imprimir los valores de las funciones
+# 
+# en 101 puntos igualmente espaciados y que cubran el intervalo [0.99, 1.01]. Calcular cada función en la
+# forma en que está escrita. Observar que las tres funciones son matemáticamente idénticas. Explique
+# porque no todos los valores son positivos. 
+
+# In[36]:
+
+
+
+def f(x):
+    return x**8 - 8*x**7 + 28*x**6 - 56*x** 5 + 70*x** 4 -56*x** 3 + 28*x**2 - 8*x + 1
+
+def g(x):
+    return (((((((x-8)*x + 28)*x - 56)*x + 70)*x - 56)*x + 28)*x - 8)*x + 1
+
+def h(x):
+    return (x - 1)**8
+
+for i in range(0, 101):
+    x = 0.99 + i*0.0002
+    print("%.4f | f(x) = %25s | g(x) = %25s | h(x) = %25s" % (x, f(x), g(x), h(x)))
+
+
+# <h2>Ejercicio 8</h2>
+
+# Formula Cuadratica: Haga un programa que evalúe las raíces con el primer algoritmo y con el segundo para
+# x^2 + bx + 1 =0
+# con b = 10, 100, 1000, 10000... Discutir los resultados obtenidos.
+
+# In[41]:
+
+
+def quadratic(a, b, c):
+    x1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2.0 * a)
+    x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2.0 * a)
+    return x1, x2
+
+def improved_Quadratic(a, b, c):
+    x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2.0 * a)
+    x1 = c / (a*x2)
+    return round(x1,4), round(x2,4)
+
+
+# In[45]:
+
+
+print('Ordinary quadratic formula: ',quadratic(1, 62.10, 1))
+
+print('Improved Quadratic: ',improved_Quadratic(1, 62.10,1))
+
+
+# In[51]:
+
+
+for x in range(1, 10):
+    b = 10**x
+    print("%15s | Quad: %30s | Imp Quad: %30s" % (b, quadratic(1,b,1), improved_Quadratic(1,b,1)))
+
+
+# In[ ]:
+
+
+
 
